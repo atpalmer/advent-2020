16fe3fc6c7a31a629be39ee740d2b6879c6758df
Andrew Palmer <atp@sdf.org>
2020-12-25 10:06:32 -0600

    Day 1 - Part 2

--- day01/main.py
+++ day01/main.py
@@ -18,9 +18,26 @@ def findpair(items, target):
             return val, needle
 
 
+def findtriplet(items, target):
+    haystack = set(items)
+    for val1 in haystack:
+        for val2 in haystack:
+            needle = target - val1 - val2
+            if needle in haystack:
+                return val1, val2, needle
+
+
 def part1(filepath):
     pair = findpair(
         items=get_line_ints(filepath),
         target=2020)
     result = pair[0] * pair[1]
+    print(result)
+
+
+def part2(filepath):
+    trip = findtriplet(
+        items=get_line_ints(filepath),
+        target=2020)
+    result = trip[0] * trip[1] * trip[2]
     print(result)
