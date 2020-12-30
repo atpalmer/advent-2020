20c6aaec59220a76d8fc66c74fcd809220283fc9
Andrew Palmer <atp@sdf.org>
2020-12-29 21:06:11 -0600

    Day 8 - Part 2

--- day08/core.py
+++ day08/core.py
@@ -6,6 +6,9 @@ class Instruction(object):
         self.op = op
         self.arg = int(arg)
 
+    def change_op(self, op):
+        return Instruction(op, self.arg)
+
 
 class Program(object):
     def __init__(self, prog):
@@ -13,6 +16,7 @@ class Program(object):
         self.lineptr = 0
         self.accumulator = 0
         self.visited = set()
+        self.terminated = False
 
     def acc(self, arg):
         self.accumulator += arg
@@ -35,10 +39,28 @@ class Program(object):
     def run(self):
         while self.run_next():
             pass
+        self.terminated = self.lineptr >= len(self.prog)
         return self.accumulator
 
 
 def part1(filepath):
     prog = Program(Instruction(*line.split(' ')) for line in file.get_lines(filepath))
     return prog.run()
+
+
+def part2(filepath):
+    instructions = [Instruction(*line.split(' ')) for line in file.get_lines(filepath)]
+    for i, ins in enumerate(instructions):
+        new = list(instructions)
+        if ins.op == 'jmp':
+            new[i] = new[i].change_op('nop')
+        elif ins.op == 'nop':
+            new[i] = new[i].change_op('jmp')
+        else:
+            continue
+        prog = Program(new)
+        result = prog.run()
+        if prog.terminated:
+            return result
+    assert False, 'Unreachable'
 
